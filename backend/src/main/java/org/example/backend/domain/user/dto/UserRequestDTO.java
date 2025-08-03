bdaa1fb6be6b493c1bfda926d9cde9f7951a3138
kimjihun <xxxjjhhh@naver.com>
2025-08-03 22:42:33 +0900

    * 백엔드 : User 도메인 Service 구현 1 close #5

---
+++ backend/src/main/java/org/example/backend/domain/user/dto/UserRequestDTO.java
@@ -0,0 +1,14 @@
+package org.example.backend.domain.user.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserRequestDTO {
+
+    private String username;
+    private String password;
+    private String nickname;
+    private String email;
+}
---
+++ backend/src/main/java/org/example/backend/domain/user/service/UserService.java
@@ -0,0 +1,81 @@
+package org.example.backend.domain.user.service;
+
+import org.example.backend.domain.user.dto.UserRequestDTO;
+import org.example.backend.domain.user.entity.UserEntity;
+import org.example.backend.domain.user.entity.UserRoleType;
+import org.example.backend.domain.user.repository.UserRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.nio.file.AccessDeniedException;
+
+@Service
+public class UserService {
+
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+
+    public UserService(PasswordEncoder passwordEncoder, UserRepository userRepository) {
+        this.passwordEncoder = passwordEncoder;
+        this.userRepository = userRepository;
+    }
+
+    // 자체 로그인 회원 가입 (존재 여부)
+    @Transactional(readOnly = true)
+    public Boolean existUser(UserRequestDTO dto) {
+        return userRepository.existsByUsername(dto.getUsername());
+    }
+
+    // 자체 로그인 회원 가입
+    @Transactional
+    public Long addUser(UserRequestDTO dto) {
+
+        if (userRepository.existsByUsername(dto.getUsername())) {
+            throw new IllegalArgumentException("이미 유저가 존재합니다.");
+        }
+
+        UserEntity entity = UserEntity.builder()
+                .username(dto.getUsername())
+                .password(passwordEncoder.encode(dto.getPassword()))
+                .isLock(false)
+                .isSocial(false)
+                .roleType(UserRoleType.USER) // 우선 일반 유저로 가입
+                .nickname(dto.getNickname())
+                .email(dto.getEmail())
+                .build();
+
+        return userRepository.save(entity).getId();
+    }
+
+    // 자체 로그인
+
+    // 자체 로그인 회원 정보 수정
+    @Transactional
+    public Long updateUser(UserRequestDTO dto) throws AccessDeniedException {
+
+        // 본인만 수정 가능 검증
+        String sessionUsername = SecurityContextHolder.getContext().getAuthentication().getName();
+        if (!sessionUsername.equals(dto.getUsername())) {
+            throw new AccessDeniedException("본인 계정만 수정 가능");
+        }
+
+        // 조회
+        UserEntity entity = userRepository.findByUsernameAndIsLockAndIsSocial(dto.getUsername(), false, false)
+                .orElseThrow(() -> new UsernameNotFoundException(dto.getUsername()));
+
+        // 회원 정보 수정
+        entity.updateUser(dto);
+
+        return userRepository.save(entity).getId();
+    }
+
+    // 자체/소셜 로그인 회원 탈퇴
+
+    // 소셜 로그인 (매 로그인시 : 신규 = 가입, 기존 = 업데이트)
+
+    // 자체/소셜 유저 정보 조회
+
+}
