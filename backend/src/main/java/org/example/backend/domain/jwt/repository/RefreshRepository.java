80df46f76f72cdd7aa444899b5dff5eecee5caab
kimjihun <xxxjjhhh@naver.com>
2025-08-07 11:53:27 +0900

    * 백엔드 : Jwt 도메인 : JwtService Refresh 토큰 메소드 추가 close #12

--- backend/src/main/java/org/example/backend/domain/jwt/repository/RefreshRepository.java
+++ backend/src/main/java/org/example/backend/domain/jwt/repository/RefreshRepository.java
@@ -2,6 +2,16 @@ package org.example.backend.domain.jwt.repository;
 
 import org.example.backend.domain.jwt.entity.RefreshEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface RefreshRepository extends JpaRepository<RefreshEntity, Long> {
+
+    Boolean existsByRefresh(String refreshToken);
+
+    @Transactional
+    void deleteByRefresh(String refresh);
+
+    @Transactional
+    void deleteByUsername(String username);
+
 }
