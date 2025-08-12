a7c2598641760be92a9e0bf03656255295ec1678
kimjihun <xxxjjhhh@naver.com>
2025-08-12 17:00:16 +0900

    * 백엔드 : 엔드포인트 예외 처리 close #24

---
+++ backend/src/main/java/org/example/backend/api/CustomControllerAdvice.java
@@ -0,0 +1,27 @@
+package org.example.backend.api;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.nio.file.AccessDeniedException;
+
+@RestControllerAdvice
+public class CustomControllerAdvice {
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<String> handleAccessDeniedException(AccessDeniedException ex) {
+        return ResponseEntity
+                .status(HttpStatus.FORBIDDEN)
+                .body("접근 권한이 없습니다.");
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleRuntimeException(RuntimeException ex) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body("잘못된 요청입니다.");
+    }
+
+}
