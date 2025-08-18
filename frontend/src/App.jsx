67ae5832b8b2981bfa1d8c2719a3966e0e9cba58
kimjihun <xxxjjhhh@naver.com>
2025-08-19 04:44:57 +0900

    * 프론트 : 라우팅 추가 ref #29

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import JoinPage from "./pages/JoinPage";
+import LoginPage from "./pages/LoginPage";
+
 import './App.css'
 
 function App() {
 
   return (
-    <>
-
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/join" element={<JoinPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
